a21bdf365831fb0104f1290b2b4b07a9a80d993d
leejinlee-kr <jinijinlee.lee@gmail.com>
2022-11-22 17:47:11 +0900

    feat : Lifecycle과 useEffect

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,5 +1,6 @@
 import {useParams} from "react-router-dom";
 import styled from 'styled-components';
+import {useEffect, useState} from "react";
 
 let Box = styled.div`
   padding : 20px;
@@ -11,18 +12,52 @@ let YellowBtn = styled.button`
   padding : 10px; 
 `;
 
-function Detail(props){
-    let { id } = useParams();
-    let product = props.shoes.find(function(x){ // props.shoes.find((x) => x.id == id ) 랑 똑같음
+function Detail(props) {
+
+    let {id} = useParams();
+    let product = props.shoes.find(function (x) { // props.shoes.find((x) => x.id == id ) 랑 똑같음
         return x.id == id
     });
+    let [count, setCount] = useState(0)
+    let [alert2, setAlert2] = useState(true)
+    let [num, setNum] = useState('')
 
+    useEffect(()=>{
+        if (isNaN(num) == true){
+            alert('그러지마세요')
+        }
+    }, [num])
+
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setAlert2(false)
+        }, 2000)
+
+        return () => {
+            clearTimeout(a)
+        }
+    })
+
+    // useEffect(()=>{ }) // 재렌더링마다 코드실행하고 싶으면
+    // userEffect(()=>{}, []) // mount시 1회 코드 실행하고 싶으면
+    // useEffect(() =>{
+    //      //unmount시 1회 코드 실행하고 싶으면
+    //  }
+    // },[])
     return (
         <div className="container">
+            {
+                alert2 == true
+                    ? <div className="alert alert-warning">
+                        2초이내 구매시 할인
+                    </div>
+                    : null
+            }
             <Box>
                 <YellowBtn bg="orange">오랜지색 버튼</YellowBtn>
                 <YellowBtn bg="blue">파란색 버튼</YellowBtn>
             </Box>
+            <button onClick={()=>{ setCount(count+1)}}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg"} width="100%"/>
@@ -33,6 +68,7 @@ function Detail(props){
                     <p>{product.price}</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
+                <input onChange={((e)=>{ setNum(e.target.value) })} />
             </div>
         </div>
     )
