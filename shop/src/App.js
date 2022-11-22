58eb3d9c367bc887eec68de5c7c85763f00f9708
leejinlee-kr <jinijinlee.lee@gmail.com>
2022-11-22 14:13:30 +0900

    feat : 이미지 넣는 법 & public 폴더 이용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -6,14 +6,33 @@ function App() {
     <div className="App">
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#features">Cart</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
+        <div className="main-bg"></div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" height="200px" />
+                    <h4>상품명</h4>
+                    <p>상품정보</p>
+                </div>
+                <div className="col-md-4">
+                    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" height="200px" />
+                    <h4>상품명</h4>
+                    <p>상품정보</p>
+                </div>
+                <div className="col-md-4">
+                    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" height="200px" />
+                    <h4>상품명</h4>
+                    <p>상품정보</p>
+                </div>
+            </div>
+        </div>
         <Button variant="primary">Primary</Button>
     </div>
   );
