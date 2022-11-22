616693474f4f331a84da754210c3036466eaf22c
leejinlee-kr <jinijinlee.lee@gmail.com>
2022-11-22 13:43:42 +0900

    feat : Bootstrap 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        <Button variant="primary">Primary</Button>
     </div>
   );
 }
