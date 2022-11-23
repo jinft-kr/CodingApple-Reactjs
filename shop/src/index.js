35d7d2b40d9634e1fdaec722a10c2ec96f2a3141
leejinlee-kr <jinijinlee.lee@gmail.com>
2022-11-23 16:24:42 +0900

    feat : redus state 변경

--- shop/src/index.js
+++ shop/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import store from './store'
+import {Provider} from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,8 +1,14 @@
 import {Table} from 'react-bootstrap'
-
+import {useDispatch, useSelector} from "react-redux";
+import { changeName } from './../store';
 function Cart(){
+
+    let state = useSelector((state) => {return state});
+    let dispatch = useDispatch()
+
     return (
         <div>
+            {state.user}의 장바구니
             <Table>
                 <thead>
                 <tr>
@@ -13,12 +19,19 @@ function Cart(){
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                </tr>
+                {
+                    state.cart.map((a, i)=>
+                        <tr key={i}>
+                            <td>1</td>
+                            <td>{state.cart[i].name}</td>
+                            <td>안녕</td>
+                            <td><button onClick={()=>{
+                                dispatch(changeName())
+                                }}>+</button>
+                            </td>
+                        </tr>
+                    )
+                }
                 </tbody>
             </Table>
         </div>
